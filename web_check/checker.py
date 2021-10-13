32f023d254a291166c5d30229ed97a1211f1231c
Jaime Álvarez <88266316+Jaime-alv@users.noreply.github.com>
2021-10-13 11:43:58 +0200

    New function def prompt_differences  - All new lines are in a new file with name whats_new.txt, inside logs folder.  - File is clear each time script is run.  - Each url stats with a hyphen.  - Everything encode with utf-8.

--- web_check/checker.py
+++ web_check/checker.py
@@ -13,6 +13,9 @@ class CompareUrl:
     def __init__(self, root, stored_url):
         self.root = root
         self.list_of_saved_url = stored_url
+        self.whats_new = pathlib.Path(f'{self.root}\\logs\\whats_new.txt')
+        with self.whats_new.open('w') as clear:
+            clear.write('')
         # iterate through all saved urls in url_list.txt
         for each_url in self.list_of_saved_url:
             self.file_name = self.list_of_saved_url[each_url]['file_name']
@@ -30,17 +33,17 @@ class CompareUrl:
         new_url = requests.get(self.url)
 
         if self.css_selector is not None:
-            self.temp_file = pathlib.Path(f'{self.root}\\url_data\\temp.txt').open('w', encoding=self.charset)
+            temp_file = pathlib.Path(f'{self.root}\\url_data\\temp.txt').open('w', encoding=self.charset)
             bs4_object = bs4.BeautifulSoup(new_url.text, features="html.parser")
             parsed_element = bs4_object.select(self.css_selector)
-            self.temp_file.write(str(parsed_element[0].get_text()))
-            self.temp_file.close()
+            temp_file.write(str(parsed_element[0].get_text()))
+            temp_file.close()
 
         elif self.css_selector is None:
-            self.temp_file = pathlib.Path(f'{self.root}\\url_data\\temp.txt').open('wb')
+            temp_file = pathlib.Path(f'{self.root}\\url_data\\temp.txt').open('wb')
             for chunk in new_url.iter_content(10000):
-                self.temp_file.write(chunk)
-            self.temp_file.close()
+                temp_file.write(chunk)
+            temp_file.close()
 
         compare_files = filecmp.cmp(f'{self.root}\\url_data\\temp.txt', self.path, shallow=False)
         if compare_files:
@@ -48,17 +51,20 @@ class CompareUrl:
         elif not compare_files:
             logging.critical(f'Opening {self.url}. Differences found.')
             webbrowser.open(self.url)
-            # todo: prompt differences in terminal
+            self.prompt_differences()
             self.save_url()
 
+    # write down differences in file
     def prompt_differences(self):
-        new_file = pathlib.Path(f'{self.root}\\url_data\\temp.txt').open('r')
+        new_file = pathlib.Path(f'{self.root}\\url_data\\temp.txt').open('r', encoding=self.charset)
         new_file_by_line = new_file.readlines()
-        old_file = pathlib.Path(f'{self.root}\\url_data\\{self.file_name}.txt').open('r')
+        old_file = pathlib.Path(f'{self.root}\\url_data\\{self.file_name}.txt').open('r', encoding=self.charset)
         old_file_by_line = old_file.readlines()
-        for line in new_file_by_line:
-            if line not in old_file_by_line:
-                print(line)
+        with self.whats_new.open('a', encoding='utf-8') as new_entrance:
+            new_entrance.write(f'- {self.url}:\n')
+            for line in new_file_by_line:
+                if line not in old_file_by_line:
+                    new_entrance.write(line)
 
     # update stored file with new text and create backup with old text
     def save_url(self):
